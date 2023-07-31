034309809363cd1cbd0b05103f1fb9d7e5a61397
SpcyR <rodchomchin7354@gmail.com>
2023-07-31 09:28:31 +0000

    part2-word_guesses

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -7,7 +7,57 @@ class WordGuesserGame
 
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
   end
+
+  attr_accessor :word, :guesses, :wrong_guesses
+
+  def guess(letter)
+    raise ArgumentError if letter.nil?
+
+    letter.downcase!
+
+    if letter.empty?
+      raise ArgumentError
+    elsif !letter.match?(/[a-zA-Z]/)
+      raise ArgumentError
+    elsif @word.include?(letter) and not @guesses.include?(letter)
+      @guesses << letter
+      return true
+    elsif not @wrong_guesses.include?(letter) and not @word.include?(letter)
+      @wrong_guesses << letter
+      return true
+    end
+    return false
+  end
+
+  def word_with_guesses
+    display = ""
+    @word.each_char do |char|
+      if @guesses.include?(char)
+        display += char
+      else
+        display += '-'
+      end
+    end
+    display
+  end
+
+  def word_guessed?
+    @word.chars.all? { |char| @guesses.include?(char) }
+  end
+
+  def check_win_or_lose
+    if word_guessed?
+      return :win
+    elsif @wrong_guesses.length >= 7
+      return :lose
+    else
+      return :play
+    end
+  end
+
 
   # You can test it by installing irb via $ gem install irb
   # and then running $ irb -I. -r app.rb
